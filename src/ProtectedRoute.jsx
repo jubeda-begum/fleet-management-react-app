98846bee9502242ab3e2fc4f5ea87461826f64e7
jubeda-begum <shaikjubedabegum50@gmail.com>
2025-12-28 19:51:43 +0530

    Update ProtectedRoute.jsx

--- src/ProtectedRoute.jsx
+++ src/ProtectedRoute.jsx
@@ -1,11 +1,5 @@
-// Import Navigate for redirect
 import { Navigate } from "react-router-dom";
-
-// Import auth object
 import { auth } from "./auth";
-
-// Protect routes based on login status
 export default function ProtectedRoute({ children }) {
-  // If authenticated show page else redirect to login
   return auth.isAuth ? children : <Navigate to="/login" />;
 }
